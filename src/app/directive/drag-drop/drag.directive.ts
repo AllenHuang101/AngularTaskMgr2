813c2655f278cedc2d54435247bcbf547aa185f9
allen3_huang <allen3_huang@asus.com>
2018-12-09 16:19:22 +0800

    update DragDrop

--- src/app/directive/drag-drop/drag.directive.ts
+++ src/app/directive/drag-drop/drag.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { DragDropService } from '../drag-drop.service';
 
 @Directive({
-  selector: '[app-draggable][draggedClass]'
+  selector: '[app-draggable][dragTag][dragData][draggedClass]'
 })
 export class DragDirective {
 
@@ -18,17 +19,21 @@ export class DragDirective {
   }
 
   @Input() draggedClass: string;
-  constructor(private el: ElementRef, private rd: Renderer2) { }
+  @Input() dragTag: string;
+  @Input() dragData: any;
+
+  constructor(private el: ElementRef, private rd: Renderer2, private service: DragDropService) { }
 
   @HostListener('dragstart', ['$event'])
-  ondragstart(ev: Event) {
+  onDragstart(ev: Event) {
     if (this.el.nativeElement === ev.target) {
       this.rd.addClass(this.el.nativeElement, this.draggedClass);
+      this.service.setDragData({ tag: this.dragTag, data: this.dragData });
     }
   }
 
   @HostListener('dragend', ['$event'])
-  ondragend(ev: Event) {
+  onDragend(ev: Event) {
     if (this.el.nativeElement === ev.target) {
       this.rd.removeClass(this.el.nativeElement, this.draggedClass);
     }
--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, HostBinding,ChangeDetectionStrategy,ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
-import { NewTaskComponent } from '../new-task/new-task.component';
-import { CopyTaskComponent } from '../copy-task/copy-task.component';
-import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
-import { NewTaskListComponent } from '../new-task-list/new-task-list.component';
 import { slideToRight } from '../../anims/router.anim';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
+import { CopyTaskComponent } from '../copy-task/copy-task.component';
+import { NewTaskListComponent } from '../new-task-list/new-task-list.component';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 @Component({
   selector: 'app-task-home',
@@ -21,11 +21,11 @@ export class TaskHomeComponent implements OnInit {
 
   lists = [
     {
-      id:1,
+      id: 1,
       name: "待办",
       tasks: [
         {
-          id:1,
+          id: 1,
           desc: '任务一：去星巴克买杯咖啡',
           completed: true,
           priority: 1,
@@ -37,7 +37,7 @@ export class TaskHomeComponent implements OnInit {
           dueDate: new Date(),
         },
         {
-          id:2,
+          id: 2,
           desc: '任务一：去星巴克买杯咖啡去星巴克买杯咖啡',
           completed: false,
           priority: 2,
@@ -52,11 +52,11 @@ export class TaskHomeComponent implements OnInit {
       ]
     },
     {
-      id:2,
+      id: 2,
       name: "待办",
       tasks: [
         {
-          id:1,
+          id: 1,
           desc: '任务一：去星巴克买杯咖啡',
           completed: true,
           priority: 3,
@@ -71,32 +71,44 @@ export class TaskHomeComponent implements OnInit {
     }
   ];
 
-  constructor(private dialog: MdDialog,private cd:ChangeDetectorRef) { }
+  constructor(private dialog: MdDialog, private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
   }
 
-  launchNewTaskDialog(){
-    const dialogRef = this.dialog.open(NewTaskComponent,{data:{title:'新建任务'}});
+  launchNewTaskDialog() {
+    const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '新建任务' } });
   }
-  launchCopyTaskDialog(){
-    const dialogRef = this.dialog.open(CopyTaskComponent,{data:{lists: this.lists}});
+  launchCopyTaskDialog() {
+    const dialogRef = this.dialog.open(CopyTaskComponent, { data: { lists: this.lists } });
   }
-  launchUpdateTaskDialog(task){
+  launchUpdateTaskDialog(task) {
     console.log(111);
-    const dialogRef = this.dialog.open(NewTaskComponent,{data:{title:'修改任务',task: task}});
+    const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '修改任务', task: task } });
   }
-  launchConfirmDialog(){
-    const dialogRef = this.dialog.open(ConfirmDialogComponent,{data:{title:'提示',content:'您确认要执行该操作吗？'}});
+  launchConfirmDialog() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '提示', content: '您确认要执行该操作吗？' } });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
-  launchEditListDialog(){
-    const dialogRef = this.dialog.open(NewTaskListComponent,{data:{title:'更改列表名称'}});
+  launchEditListDialog() {
+    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '更改列表名称' } });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
-  launchNewListDialog(){
-    const dialogRef = this.dialog.open(NewTaskListComponent,{data:{title:'新增列表名称'}});
+  launchNewListDialog() {
+    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '新增列表名称' } });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
+  handleMove(srcData, list) {
+    switch (srcData.tag) {
+      case 'task-item':
+        console.log('handling item');
+        break;
+      case 'task-list':
+        console.log('handling list');
+        break;
+      default:
+        break;
+    }
+  }
 }
